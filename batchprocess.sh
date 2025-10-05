21964d87246f22f031b1c9f3fc9af166250413f1
Stefano Moia <s.moia.research@gmail.com>
2025-10-06 00:15:49 +0200

    Batchprocess (daBeast version)

--- batchprocess.sh
+++ batchprocess.sh
@@ -1,9 +1,30 @@
 #!/usr/bin/env bash
 
 # apptainer shell --no-home -e -B /Neurodata/M3PI:/data -B $GITLAB/M3PI_preproc:/scripts -B /scratch:/tmp M3PI_vessels.sif
+# for sub in $(seq -f %02g 2 6)
+# do
+# /scripts/3dMEEPI_preproc.sh -anat /data/sub-${sub}/ses-7T/anat/sub-${sub}_ses-7T_acq-normRO_run-1_echo-1_part-mag_T2starw.nii.gz
+# done
 
-for sub in $( seq 1 6 );
+# apptainer shell --no-home -e -B /Neurodata/M3PI:/data -B $GITLAB/M3PI_preproc:/scripts -B /scratch:/tmp VesselBoost.sif
+# for sub in $(seq -f %02g 2 6)
+# do
+# /scripts/vessels_segmentation.sh -anat /data/derivatives/vessels/sub-${sub}/ses-7T/anat/00.sub-${sub}_ses-7T_part-mag_T2starw_imgavg_preprocessed
+# done
+
+for sub in $(seq -f %02g 1 6)
 do
-	/scripts/3dMEEPI_preproc.sh -anat /data/sub-01/ses-7T/anat/sub-01_ses-7T_acq-normRO_run-1_echo-1_part-mag_T2starw.nii.gz
+screen_name="vesselprep_${sub}"
+
+screen -dmS "$screen_name" \
+-L -Logfile /Neurodata/Logs/${screen_name}.log \
+bash -c "
+apptainer exec -e --no-home -B /Neurodata/M3PI:/data -B $GITLAB/M3PI_preproc:/scripts -B /scratch:/tmp M3PI_vessels.sif \
+/scripts/3dMEEPI_preproc.sh -anat /data/sub-${sub}/ses-7T/anat/sub-${sub}_ses-7T_acq-normRO_run-1_echo-1_part-mag_T2starw.nii.gz;
+apptainer exec -e --no-home -B /Neurodata/M3PI:/data -B $GITLAB/M3PI_preproc:/scripts -B /scratch:/tmp vesselboost.sif \
+/scripts/vessels_segmentation.sh -anat /data/derivatives/vessels/sub-${sub}/ses-7T/anat/00.sub-${sub}_ses-7T_part-mag_T2starw_imgavg_preprocessed;
+"
 done
+
+
 
