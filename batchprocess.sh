06494575005e5ea45d66fc9f8d221cbaf1f780dc
Stefano Moia <s.moia.research@gmail.com>
2025-10-06 02:10:33 +0200

    Real dabeast

--- batchprocess.sh
+++ batchprocess.sh
@@ -19,9 +19,9 @@ screen_name="vesselprep_${sub}"
 screen -dmS "$screen_name" \
 -L -Logfile /Neurodata/Logs/${screen_name}.log \
 bash -c "
-apptainer exec -e --no-home -B /Neurodata/M3PI:/data -B $GITLAB/M3PI_preproc:/scripts -B /scratch:/tmp M3PI_vessels.sif \
+apptainer exec -e --no-home -B $M3PI:/data -B $GITLAB/M3PI_preproc:/scripts -B /tmp:/tmp $GITLAB/M3PI_preproc/M3PI_vessels.sif \
 /scripts/3dMEEPI_preproc.sh -anat /data/sub-${sub}/ses-7T/anat/sub-${sub}_ses-7T_acq-normRO_run-1_echo-1_part-mag_T2starw.nii.gz;
-apptainer exec -e --no-home -B /Neurodata/M3PI:/data -B $GITLAB/M3PI_preproc:/scripts -B /scratch:/tmp vesselboost.sif \
+apptainer exec -e --no-home -B $M3PI:/data -B $GITLAB/M3PI_preproc:/scripts -B /tmp:/tmp $GITLAB/M3PI_preproc/VesselBoost.sif \
 /scripts/vessels_segmentation.sh -anat /data/derivatives/vessels/sub-${sub}/ses-7T/anat/00.sub-${sub}_ses-7T_part-mag_T2starw_imgavg_preprocessed;
 "
 done
